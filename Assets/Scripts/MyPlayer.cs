d10ff794033135ab9c2f84a0f1abd1fe42e1832b
Adrien <adrien.charneau@gmail.com>
2020-10-23 14:27:16 +0200

    cours avant pause dej

--- Assets/Scripts/MyPlayer.cs
+++ Assets/Scripts/MyPlayer.cs
@@ -7,13 +7,21 @@ public class MyPlayer : MonoBehaviour
     //  Syntaxe 'PascalCase' pour paramêtres de classe
     [SerializeField] private float m_TranslationSpeed;
 
+    Transform m_Transform;
+
     // Getters et setters = Propriétés (en C#)
     public float GetTranslationSpeed()
     {
         return m_TranslationSpeed;
     }
 
+    // Initializations qui concernent que l'objet lui-même
+    private void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
 
+    // Initializations qui concernent les objets exterieurs (s'exécute après l'execution de tous les awake appartenant à tous les objets de la scene)
     void Start()
     {
         //  Syntaxe 'camelCase' pour variables locales
@@ -22,6 +30,19 @@ public class MyPlayer : MonoBehaviour
 
     void Update()
     {
+        // .forward = vecteur donné dans le référentiel global
+        Vector3 moveVect = transform.forward * m_TranslationSpeed * m_TranslationSpeed * Time.deltaTime;
+
+        Vector3 moveVectLocal = new Vector3(0,0,1) * m_TranslationSpeed * m_TranslationSpeed * Time.deltaTime;
+        moveVectLocal = Vector3.forward * m_TranslationSpeed * m_TranslationSpeed * Time.deltaTime;
+
+        //  transform.position += moveVect;
+
+        //  plus performant que le simple 'transform' (m_Transform est mis en cache):
+        //m_Transform.position += moveVect;
         
+
+        //m_Transform.Translate(moveVect, Space.World);
+        m_Transform.Translate(moveVectLocal, Space.Self);
     }
 }
