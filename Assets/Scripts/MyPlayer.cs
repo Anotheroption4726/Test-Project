9506c9386ee8decac04ab26cc84a188c711b2e3b
Adrien <adrien.charneau@gmail.com>
2020-10-23 12:21:33 +0200

    texture et syntaxe C#

---
+++ Assets/Scripts/MyPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    //  Syntaxe 'PascalCase' pour paramêtres de classe
+    [SerializeField] private float m_TranslationSpeed;
+
+    // Getters et setters = Propriétés (en C#)
+    public float GetTranslationSpeed()
+    {
+        return m_TranslationSpeed;
+    }
+
+
+    void Start()
+    {
+        //  Syntaxe 'camelCase' pour variables locales
+        float myVariable;
+    }
+
+    void Update()
+    {
+        
+    }
+}
