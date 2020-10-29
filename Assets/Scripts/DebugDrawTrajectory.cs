6274db8eae59481cb9b3ee21b7911ad5012a3b45
Adrien <napalm_ground@hotmail.com>
2020-10-29 14:56:33 +0100

    mouse raycast

--- Assets/Scripts/DebugDrawTrajectory.cs
+++ Assets/Scripts/DebugDrawTrajectory.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class DebugDrawTrajectory : MonoBehaviour
 {
-    [SerializeField] int m_NMaxPositions;
-    [SerializeField] int m_NDisplayedPositions;
-    [SerializeField] int m_MinDistBetweenPos;
-    [SerializeField] Color m_DisplayColor;
-    Vector3[] m_Positions;
+    [SerializeField] int m_NMaxPositions; //    nombre maximum de positions stockables
+    [SerializeField] int m_NDisplayedPositions; //    nombre maximum de positions affichées (on affiche les dernières)
+    [SerializeField] int m_MinDistBetweenPos;   //  la distance minimum entre deux positions pour stockage
+    [SerializeField] Color m_DisplayColor;  // couleur d'affichage
+    Vector3[] m_Positions;  // décalage de position pour affichage
 
     int m_CurrentIndex = 0;
 
@@ -22,6 +22,7 @@ public class DebugDrawTrajectory : MonoBehaviour
 
     private void Update()
     {
+        // enregistrement des positions successives
         if(m_CurrentIndex < m_Positions.Length - 1)
         {
             Vector3 pos = m_Transform.position;
@@ -32,15 +33,34 @@ public class DebugDrawTrajectory : MonoBehaviour
             }
         }
 
+        //  affichage de la trajectoire
         if(m_CurrentIndex >= 1)
         {
-            int startIndex = Mathf.Max(0, m_CurrentIndex - m_NDisplayedPositions);
+            int startIndex = Mathf.Max(0, m_CurrentIndex - m_NDisplayedPositions);  // on affiche les dernières positions stockées
 
             for (int i = startIndex; i < m_CurrentIndex; i++)
             {
                 Debug.DrawLine(m_Positions[i], m_Positions[i + 1], m_DisplayColor, 0);
 
             }
-        } 
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //  affichage de la trajectoire
+        if (m_CurrentIndex >= 1)
+        {
+            int startIndex = Mathf.Max(0, m_CurrentIndex - m_NDisplayedPositions);  // on affiche les dernières positions stockées
+
+            for (int i = startIndex; i < m_CurrentIndex; i++)
+            {
+                Gizmos.color = m_DisplayColor;
+                Gizmos.DrawLine(m_Positions[i], m_Positions[i + 1]);
+
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(m_Positions[i], 0.025f);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/MouseRaycast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRaycast : MonoBehaviour
+{
+    [SerializeField] LayerMask m_SelectionLayerMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction*1000f);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray.origin, ray.direction, out hit, float.PositiveInfinity, m_SelectionLayerMask.value))
+        {
+            GameObject go = hit.collider.gameObject;
+            MeshRenderer mr = go.GetComponent<MeshRenderer>();
+            if (mr)
+            {
+                mr.material.color = Random.ColorHSV();
+            }
+        }
+    }
+}
